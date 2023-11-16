7b2b87a6b79ee25875920d5e5d10341904b92136
StrohXl <xaviermayora20@gmail.com>
2023-11-16 11:43:50 -0400

    added calendars

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-layout
+
+  >
+  <Calendario/>
+  </v-layout>
+</template>
+
+<script>
+import Calendario from '~/components/Calendario.vue';
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo,
+    Calendario
+  }
+}
+</script>
---
+++ pages/trabajo/_id.vue
@@ -0,0 +1,13 @@
+<template lang="">
+  <div>
+    Trabajo {{this.$route.params.id}}
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+
+</style>
