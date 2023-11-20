7aa0f8a1af7313a03ca49b72a8cb4656b8ff2425
O'Dell <ingsoftkar@hotmail.com>
2023-11-20 09:50:39 -0400

    added notifications

---
+++ components/shared/notifications.vue
@@ -0,0 +1,153 @@
+<template>
+  <v-badge
+    v-if="count>0"
+    color="red"
+    :content="count"
+    class="mx-6 mt-3 cursor-pointer barlowc-sb"
+    offset-y="7"
+  >
+    <v-menu
+      bottom
+      offset-y
+      left
+    >
+      <template #activator="{ on, attrs }">
+        <v-icon
+          v-bind="attrs"
+          class="rotate-notification mt-n3 mr-n1 cursor-pointer"
+          size="30"
+          :color="expert?'#003566': ''"
+          v-on="on"
+        >
+          far fa-bell
+        </v-icon>
+      </template>
+      <div class="white pa-md-4" :style="`width:${$vuetify.breakpoint.mdAndUp?'400px':'250px'}`">
+        <div class="d-flex align-baseline">
+          <div class="rmh barlowc-m">
+            Notificaciones
+          </div>
+          <v-spacer />
+          <div class="f13 cursor-pointer blueItem rubik-r" @click="count=0">
+            Eliminar notificaciones
+          </div>
+        </div>
+        <v-container class="pa-0 mt-md-3">
+          <v-card
+            v-for="(item, n) in notifications"
+            :key="n"
+            flat
+            height="66"
+            class="d-flex elevation-0"
+          >
+            <v-avatar size="36" class="mr-4 borderAvatar">
+              <v-img :src="item.avatar" />
+            </v-avatar>
+            <div style="height: 100%">
+              <div class="mt-1">
+                <p class="f13 colorBlue rubik-r ma-0">
+                  {{ item.name + ' ' + item.last_name + '. ' + item.category + '. ' }}
+                  <span class="labelGray f13">{{ item.description }}</span>
+                </p>
+              </div>
+              <div class="text-left f12 rubik-r" style="color: #7E8489">
+                {{ item.date }}
+              </div>
+            </div>
+            <v-spacer />
+            <div v-if="item.is_new" class="mt-2">
+              <v-badge
+                color="red"
+                offset-x="16"
+                offset-y="12"
+                overlap
+                dot
+              >
+                <v-icon class="rotate-notification mr-2" size="20">
+                  mdi-bell-outline
+                </v-icon>
+              </v-badge>
+            </div>
+            <div v-else class="mt-2">
+              <v-icon class="rotate-notification mr-2" size="20">
+                mdi-bell-outline
+              </v-icon>
+            </div>
+          </v-card>
+        </v-container>
+      </div>
+    </v-menu>
+  </v-badge>
+</template>
+
+<script>
+
+export default {
+  name: 'Notifications',
+  props: {
+    expert: Boolean,
+    notifications:
+    {
+      type: Array,
+      default: () => [
+        {
+          name: 'asd',
+          last_name: 'qwe',
+          category: 'Informático',
+          is_new: true,
+          avatar: 'https://randomuser.me/api/portraits/women/81.jpg',
+          description: 'Te ha enviado una solicituddfv,ndfg nrkgn kel;gn klerng ke kerfn gkebn',
+          date: '4 de diciembre del 2022'
+        },
+        {
+          name: 'asd',
+          last_name: 'qwe',
+          category: 'Informático',
+          is_new: true,
+          avatar: 'https://randomuser.me/api/portraits/women/81.jpg',
+          description: 'Te ha enviado una solicitud',
+          date: '4 de diciembre del 2022'
+        },
+        {
+          name: 'asd',
+          last_name: 'qwe',
+          category: 'Informático',
+          is_new: false,
+          avatar: 'https://randomuser.me/api/portraits/women/81.jpg',
+          description: 'Te ha enviado una solicitud',
+          date: '4 de diciembre del 2022'
+        },
+        {
+          name: 'asd',
+          last_name: 'qwe',
+          category: 'Informático',
+          is_new: false,
+          avatar: 'https://randomuser.me/api/portraits/women/81.jpg',
+          description: 'Te ha enviado una solicitud',
+          date: '4 de diciembre del 2022'
+        },
+        {
+          name: 'asd',
+          last_name: 'qwe',
+          category: 'Informático',
+          is_new: false,
+          avatar: 'https://randomuser.me/api/portraits/women/81.jpg',
+          description: 'Te ha enviado una solicitud',
+          date: '4 de diciembre del 2022'
+        }
+      ]
+    }
+  },
+  data () {
+    return {
+      count: 2
+    }
+  }
+}
+</script>
+
+<style scoped>
+.rotate-notification {
+  transform: rotate(15deg) !important;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -56,6 +56,7 @@
       >
         <v-icon>mdi-menu</v-icon>
       </v-btn>
+      <sharedNotifications :notifications="notifications" />
     </v-app-bar>
     <v-main>
       <v-container>
@@ -105,6 +106,116 @@ export default {
           icon: 'mdi-chart-bubble',
           title: 'Inspire',
           to: '/inicio'
+        }
+      ],
+      notifications: [
+        {
+          name: 'Jose',
+          last_name: 'Martinez',
+          category: 'Electricista',
+          is_new: true,
+          avatar: 'https://randomuser.me/api/portraits/men/32.jpg',
+          description: 'Te ha enviado una cotización',
+          date: '2 de febrero. 12:32 p.m.'
+        },
+        {
+          name: 'Anderson',
+          last_name: 'Moreno',
+          category: 'Plomero',
+          is_new: true,
+          avatar: 'https://randomuser.me/api/portraits/men/33.jpg',
+          description: 'Te ha rechazado una cotizacion',
+          date: '22 de febrero, 09:27 a.m.'
+        },
+        {
+          name: 'Henry',
+          last_name: 'Fernandez',
+          category: 'Carpintero',
+          is_new: false,
+          avatar: 'https://randomuser.me/api/portraits/men/34.jpg',
+          description: 'Te ha programado una visita presencial',
+          date: '21 de febrero, 08:21 p.m.'
+        },
+        {
+          name: 'Jose',
+          last_name: 'Martinez',
+          category: 'Electricista',
+          is_new: false,
+          avatar: 'https://randomuser.me/api/portraits/men/32.jpg',
+          description: 'Te ha enviado una cotización',
+          date: '2 de febrero. 12:32 p.m.'
+        },
+        {
+          name: 'Anderson',
+          last_name: 'Moreno',
+          category: 'Plomero',
+          is_new: false,
+          avatar: 'https://randomuser.me/api/portraits/men/33.jpg',
+          description: 'Te ha rechazado una cotizacion',
+          date: '22 de febrero, 09:27 a.m.'
+        },
+        {
+          name: 'Henry',
+          last_name: 'Fernandez',
+          category: 'Carpintero',
+          is_new: false,
+          avatar: 'https://randomuser.me/api/portraits/men/34.jpg',
+          description: 'Te ha programado una visita presencial',
+          date: '21 de febrero, 08:21 p.m.'
+        },
+        {
+          name: 'Jose',
+          last_name: 'Martinez',
+          category: 'Electricista',
+          is_new: true,
+          avatar: 'https://randomuser.me/api/portraits/men/32.jpg',
+          description: 'Te ha enviado una cotización',
+          date: '2 de febrero. 12:32 p.m.'
+        },
+        {
+          name: 'Anderson',
+          last_name: 'Moreno',
+          category: 'Plomero',
+          is_new: true,
+          avatar: 'https://randomuser.me/api/portraits/men/33.jpg',
+          description: 'Te ha rechazado una cotizacion',
+          date: '22 de febrero, 09:27 a.m.'
+        },
+        {
+          name: 'Henry',
+          last_name: 'Fernandez',
+          category: 'Carpintero',
+          is_new: false,
+          avatar: 'https://randomuser.me/api/portraits/men/34.jpg',
+          description: 'Te ha programado una visita presencial',
+          date: '21 de febrero, 08:21 p.m.'
+        },
+        {
+          name: 'Jose',
+          last_name: 'Martinez',
+          category: 'Electricista',
+          is_new: false,
+          avatar: 'https://randomuser.me/api/portraits/men/32.jpg',
+          description: 'Te ha enviado una cotización',
+          date: '2 de febrero. 12:32 p.m.'
+        },
+        {
+          name: 'Anderson',
+          last_name: 'Moreno',
+          category: 'Plomero',
+          is_new: false,
+          avatar: 'https://randomuser.me/api/portraits/men/33.jpg',
+          description: 'Te ha rechazado una cotizacion',
+          date: '22 de febrero, 09:27 a.m.'
+        },
+        {
+          name: 'Henry',
+          last_name: 'Fernandez',
+          category: 'Carpintero',
+          is_new: false,
+          avatar: 'https://randomuser.me/api/portraits/men/34.jpg',
+          description: 'Te ha programado una visita presencial',
+          date: '21 de febrero, 08:21 p.m.'
         }
       ],
       miniVariant: false,
